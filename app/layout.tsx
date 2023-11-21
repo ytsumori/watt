aa14bc3db18f61704f6523e2a4d6e6189e2e0f88
Yuta Tsumori <yutatsumori@gmail.com>
2023-11-21 11:27:51 +0900

    add Google Map to Home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
-  metadataBase: new URL('https://postgres-prisma.vercel.app'),
-  title: 'Vercel Postgres Demo with Prisma',
-  description:
-    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
-}
+  metadataBase: new URL("https://postgres-prisma.vercel.app"),
+  title: "Senbero",
+  description: "My very first senbero app",
+};
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.variable}>{children}</body>
     </html>
-  )
+  );
 }
