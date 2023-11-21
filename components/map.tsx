9af0cd4ae3bada5d1530f379dfde5e2fa012e602
Yuta Tsumori <yutatsumori@gmail.com>
2023-11-21 18:16:16 +0900

    manage restaurant selection in state

--- components/map.tsx
+++ components/map.tsx
@@ -6,6 +6,7 @@ import {
   Children,
   cloneElement,
   isValidElement,
+  useCallback,
   useEffect,
   useRef,
   useState,
@@ -21,11 +22,25 @@ export default function Map({ restaurants }: { restaurants: Restaurant[] }) {
     lat: 34.67932578786201,
     lng: 135.4961550080261,
   });
+  const [selectedRestaurantID, setSelectedRestaurantID] = useState<number>();
 
-  const onIdle = (map: google.maps.Map) => {
-    setZoom(map.getZoom()!);
-    setCenter(map.getCenter()!.toJSON());
-  };
+  const handleIdle = useCallback(
+    (map: google.maps.Map) => {
+      if (zoom !== map.getZoom()) {
+        setZoom(map.getZoom()!);
+      }
+      if (
+        JSON.stringify(center) !== JSON.stringify(map.getCenter()!.toJSON())
+      ) {
+        setCenter(map.getCenter()!.toJSON());
+      }
+    },
+    [center, zoom]
+  );
+
+  const handleRestaurantSelect = useCallback((restaurantID: number) => {
+    setSelectedRestaurantID(restaurantID);
+  }, []);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -53,7 +68,7 @@ export default function Map({ restaurants }: { restaurants: Restaurant[] }) {
       <MapComponent
         className="w-screen h-screen"
         center={center}
-        onIdle={onIdle}
+        onIdle={handleIdle}
         zoom={zoom}
       >
         {restaurants.map((restaurant) => (
@@ -61,6 +76,8 @@ export default function Map({ restaurants }: { restaurants: Restaurant[] }) {
             key={restaurant.id}
             position={{ lat: restaurant.latitude, lng: restaurant.longitude }}
             title={restaurant.name}
+            selected={restaurant.id === selectedRestaurantID}
+            onClick={() => handleRestaurantSelect(restaurant.id)}
             clickable
           />
         ))}
@@ -71,7 +88,7 @@ export default function Map({ restaurants }: { restaurants: Restaurant[] }) {
 
 interface MapProps extends google.maps.MapOptions {
   style?: { [key: string]: string };
-  onIdle?: (map: google.maps.Map) => void;
+  onIdle: (map: google.maps.Map) => void;
   children?: React.ReactNode;
   className?: string;
 }
@@ -98,6 +115,13 @@ function MapComponent({
     }
   }, [map, options]);
 
+  useEffect(() => {
+    if (map) {
+      google.maps.event.clearListeners(map, "idle");
+      map.addListener("idle", () => onIdle(map));
+    }
+  }, [map, onIdle]);
+
   return (
     <>
       <div ref={ref} id="map" style={style} className={className} />
@@ -112,9 +136,16 @@ function MapComponent({
   );
 }
 
-function Marker(options: google.maps.MarkerOptions) {
+interface MarkerProps extends google.maps.MarkerOptions {
+  selected: boolean;
+  onClick: () => void;
+}
+
+function Marker({ selected, onClick, ...options }: MarkerProps) {
   const [marker, setMarker] = useState<google.maps.Marker>();
   const [infoWindow, setInfoWindow] = useState<google.maps.InfoWindow>();
+
+  const handleClick = useCallback(onClick, [onClick]);
 
   useEffect(() => {
     if (!infoWindow) {
@@ -137,15 +168,35 @@ function Marker(options: google.maps.MarkerOptions) {
 
   useEffect(() => {
     if (marker) {
-      marker.setOptions(options);
-      if (infoWindow) {
-        marker.addListener("click", () => {
-          infoWindow.close();
-          infoWindow.setContent(options.title);
-          infoWindow.open(marker.getMap(), marker);
-        });
+      if (selected) {
+        const selectedIcon: google.maps.Symbol = {
+          path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+          fillColor: "blue",
+          fillOpacity: 1,
+          strokeWeight: 0,
+          scale: 2,
+          anchor: new google.maps.Point(0, 20),
+        };
+        marker.setIcon(selectedIcon);
+      } else {
+        marker.setIcon(undefined);
       }
     }
-  }, [marker, options, infoWindow]);
+  }, [marker, selected]);
+
+  useEffect(() => {
+    if (marker) {
+      marker.setOptions(options);
+    }
+  }, [marker, options]);
+
+  useEffect(() => {
+    if (marker) {
+      google.maps.event.clearListeners(marker, "click");
+      marker.addListener("click", () => {
+        onClick();
+      });
+    }
+  }, [marker, onClick]);
   return null;
 }
