c39817a08b378d26b05c7bbeafbcbc1b54de75f9
Yuta Tsumori <yutatsumori@gmail.com>
2024-01-19 10:22:49 +0900

    add component theme

--- lib/chakra-ui/theme.ts
+++ lib/chakra-ui/theme.ts
@@ -1,10 +1,30 @@
-import { defineStyleConfig, extendTheme } from "@chakra-ui/react";
+import { switchAnatomy, tabsAnatomy } from "@chakra-ui/anatomy";
+import {
+  createMultiStyleConfigHelpers,
+  defineStyleConfig,
+  extendTheme,
+} from "@chakra-ui/react";
+
+const { defineMultiStyleConfig: defineTabsStyleConfig } =
+  createMultiStyleConfigHelpers(tabsAnatomy.keys);
+const { defineMultiStyleConfig: defineSwitchStyleConfig } =
+  createMultiStyleConfigHelpers(switchAnatomy.keys);
 
 export const theme = extendTheme({
   components: {
     Button: defineStyleConfig({
       defaultProps: {
         size: "sm",
+        colorScheme: "cyan",
+      },
+    }),
+    Tabs: defineTabsStyleConfig({
+      defaultProps: {
+        colorScheme: "cyan",
+      },
+    }),
+    Switch: defineSwitchStyleConfig({
+      defaultProps: {
         colorScheme: "cyan",
       },
     }),
