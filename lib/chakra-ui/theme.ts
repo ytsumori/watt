8c29e8bf554acc59d450c2cdebcd8e7bcb0ba97c
Yuta Tsumori <yutatsumori@gmail.com>
2024-01-27 13:08:23 +0900

    apply theme color to all components

--- lib/chakra-ui/theme.ts
+++ lib/chakra-ui/theme.ts
@@ -1,38 +1,25 @@
-import { switchAnatomy, tabsAnatomy } from "@chakra-ui/anatomy";
 import {
-  createMultiStyleConfigHelpers,
   defineStyleConfig,
   extendTheme,
+  withDefaultColorScheme,
 } from "@chakra-ui/react";
 
-const { defineMultiStyleConfig: defineTabsStyleConfig } =
-  createMultiStyleConfigHelpers(tabsAnatomy.keys);
-const { defineMultiStyleConfig: defineSwitchStyleConfig } =
-  createMultiStyleConfigHelpers(switchAnatomy.keys);
-
-export const theme = extendTheme({
-  components: {
-    Button: defineStyleConfig({
-      defaultProps: {
-        size: "sm",
-        colorScheme: "cyan",
-      },
-    }),
-    Tabs: defineTabsStyleConfig({
-      defaultProps: {
-        colorScheme: "cyan",
-      },
-    }),
-    Switch: defineSwitchStyleConfig({
-      defaultProps: {
-        colorScheme: "cyan",
-      },
-    }),
-    Progress: defineStyleConfig({
-      defaultProps: {
-        size: "sm",
-        colorScheme: "cyan",
-      },
-    }),
-  },
-});
+export const theme = extendTheme(
+  withDefaultColorScheme({
+    colorScheme: "cyan",
+  }),
+  {
+    components: {
+      Button: defineStyleConfig({
+        defaultProps: {
+          size: "sm",
+        },
+      }),
+      Progress: defineStyleConfig({
+        defaultProps: {
+          size: "sm",
+        },
+      }),
+    },
+  }
+);
