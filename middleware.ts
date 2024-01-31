52fe014d7c9feac4c8d6613e40e61958998ca69b
Yuta Tsumori <yutatsumori@gmail.com>
2024-01-31 16:16:08 +0900

    disable nextauth on admin page

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/[^/]((?!api|login|restaurant-dashboard).*)"],
+  matcher: ["/[^/]((?!api|login|restaurant-dashboard|admin).*)"],
 };
