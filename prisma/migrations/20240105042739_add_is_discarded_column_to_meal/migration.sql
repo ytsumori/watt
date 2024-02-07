cf75e6a80088b23746809214cec62765da32e63e
Yuta Tsumori <yutatsumori@gmail.com>
2024-02-07 17:06:57 +0900

    recreate migration file

--- prisma/migrations/20240105042739_add_is_discarded_column_to_meal/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "Meal_restaurantId_key";
-
--- AlterTable
-ALTER TABLE "Meal" ADD COLUMN     "isDiscarded" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20240112015716_add_is_open_to_restaurant/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Restaurant" ADD COLUMN     "isOpen" BOOLEAN NOT NULL DEFAULT false;
