ded00d4d6350f2f2d5618feadf6fb949a985754e
Yuta Tsumori <yutatsumori@gmail.com>
2024-02-08 12:32:13 +0900

    fix type error

--- actions/paypay-customer.ts
+++ actions/paypay-customer.ts
@@ -6,6 +6,8 @@ import { getMyId } from "./me";
 export async function createPaypayCustomer(paypayUserAuthorizationId: string) {
   const userId = await getMyId();
 
+  if (!userId) throw new Error("User not found");
+
   return await prisma.paypayCustomer.create({
     data: { userId, paypayUserAuthorizationId },
   });
