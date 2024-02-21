3de18b970aa8f7f8a9f920a57aa8381265112b7d
Yuta Tsumori <yutatsumori@gmail.com>
2024-02-21 16:03:28 +0900

    disable over scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="jp">
-      <body style={{ overscrollBehaviorY: "none" }}>
+      <body style={{ overscrollBehavior: "none" }}>
         <ChakraProviders>{children}</ChakraProviders>
       </body>
     </html>
