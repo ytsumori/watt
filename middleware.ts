d7a1b4baabd629143a072a1cb7dd164cfadb1e23
Yuta Tsumori <yutatsumori@gmail.com>
2024-02-22 17:32:15 +0900

    remove middleware function

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  matcher: [
-    "/((?!api|restaurant-dashboard|admin|_next/static|_next/image|favicon.ico).*)",
-  ],
-};
