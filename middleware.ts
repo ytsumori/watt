c4b583555b44e6395b9bca619d180706c3588a3f
Yuta Tsumori <yutatsumori@gmail.com>
2024-02-22 17:21:50 +0900

    fix middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/[^/]((?!api|login|restaurant-dashboard|admin).*)"],
+  matcher: [
+    "/((?!api|restaurant-dashboard|admin|_next/static|_next/image|favicon.ico).*)",
+  ],
 };
