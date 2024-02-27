c490cb667c8cb9e5d0987b8e5e87eabe9a3d23dc
Yuta Tsumori <yutatsumori@gmail.com>
2024-02-27 15:35:50 +0900

    change migration file

--- prisma/migrations/20240227061918_rename_cancelled_status/migration.sql
+++ prisma/migrations/20240227061918_rename_cancelled_status/migration.sql
@@ -6,11 +6,5 @@
 */
 -- AlterEnum
 BEGIN;
-CREATE TYPE "OrderStatus_new" AS ENUM ('PREAUTHORIZED', 'COMPLETE', 'CANCELLED');
-ALTER TABLE "Order" ALTER COLUMN "status" DROP DEFAULT;
-ALTER TABLE "Order" ALTER COLUMN "status" TYPE "OrderStatus_new" USING ("status"::text::"OrderStatus_new");
-ALTER TYPE "OrderStatus" RENAME TO "OrderStatus_old";
-ALTER TYPE "OrderStatus_new" RENAME TO "OrderStatus";
-DROP TYPE "OrderStatus_old";
-ALTER TABLE "Order" ALTER COLUMN "status" SET DEFAULT 'PREAUTHORIZED';
+ALTER TYPE name RENAME VALUE 'CANCELED' TO 'CANCELLED';
 COMMIT;
