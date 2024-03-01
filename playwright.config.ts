e43c8764c2227cd78c1e73b687f67527f097c2ba
miso-devel <kengo279@icloud.com>
2024-03-01 13:33:54 +0900

    mobileのchromeとsafariのテストを行うようにする

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,10 +32,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
+    // {
+    //   name: "chromium",
+    //   use: { ...devices["Desktop Chrome"] },
+    // },
     // {
     //   name: "firefox",
     //   use: { ...devices["Desktop Firefox"] },
@@ -46,15 +46,14 @@ export default defineConfig({
     //   use: { ...devices["Desktop Safari"] },
     // },
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
+    },
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
