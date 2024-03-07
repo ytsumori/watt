4b52a45f321e79a6e1162ffd5dc158dc739f862b
miso-devel <kengo279@icloud.com>
2024-03-07 14:56:57 +0900

    add vercel autopreview link

--- app/admin/_components/GoogleLoginButton.tsx
+++ app/admin/_components/GoogleLoginButton.tsx
@@ -11,7 +11,9 @@ export const GoogleLoginButton: FC = () => {
   const signInWithGoogle = async () => {
     const { data } = await clientSupabase.auth.signInWithOAuth({
       provider: "google",
-      options: { redirectTo: `${process.env.NEXT_PUBLIC_HOST_URL}/api/auth/callback` },
+      options: {
+        redirectTo: `${process.env.NEXT_PUBLIC_HOST_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/callback`,
+      },
     });
     if (data.url) router.push(data.url);
   };
