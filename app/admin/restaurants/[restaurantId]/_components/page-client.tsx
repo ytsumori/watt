4363bd506a406e405e3c24f5034e9e7b4b6fecb6
Yuta Tsumori <yutatsumori@gmail.com>
2024-03-14 12:42:13 +0900

    Add RestaurantDetailPage component

---
+++ app/admin/restaurants/[restaurantId]/_components/page-client.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Box, Heading } from "@chakra-ui/react";
+import { Prisma } from "@prisma/client";
+import { RestaurantBankAccount } from "./RestaurantBankAccount";
+import { MealList } from "@/components/meal/meal-list";
+
+type Props = {
+  restaurant: Prisma.RestaurantGetPayload<{ select: { id: true; name: true; bankAccount: true; meals: true } }>;
+};
+
+export function RestaurantDetailPage({ restaurant }: Props) {
+  return (
+    <Box p={10}>
+      <Heading as="h1" size="lg">
+        {restaurant.name}
+      </Heading>
+      <MealList restaurantId={restaurant.id} defaultMeals={restaurant.meals} />
+      {restaurant.bankAccount && <RestaurantBankAccount restaurantBankAccount={restaurant.bankAccount} />}
+    </Box>
+  );
+}
