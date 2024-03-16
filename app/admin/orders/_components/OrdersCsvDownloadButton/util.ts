69f3a55745ffd4171c6d33c3ac990b5b4275a117
miso-devel <kengo279@icloud.com>
2024-03-16 21:15:10 +0900

    add isValidHolderName util

--- app/admin/orders/_components/OrdersCsvDownloadButton/util.ts
+++ app/admin/orders/_components/OrdersCsvDownloadButton/util.ts
@@ -1,6 +1,15 @@
 import Encoding from "encoding-japanese";
 import Papa from "papaparse";
 
+export const isValidHolderName = (holderName: string): boolean => {
+  const splittedName = holderName.split("");
+  const regex = new RegExp(/[ｦ-ﾟ]/);
+  const isValid = splittedName.every((name) =>
+    name.match(regex) && name !== " "
+  );
+  return isValid;
+};
+
 export const convertToBlob = (records: (string | number)[][]): Blob => {
   const config = { delimiter: ",", header: true, newline: "\r\n" };
   const delimiterString = Papa.unparse(records, config);
