11b088277f830cbcca1f93d6354647fb101d316f
Yuta Tsumori <yutatsumori@gmail.com>
2024-03-19 14:30:38 +0900

    Add authorization check in GET route

--- app/api/cron/update-restaurant-status/route.ts
+++ app/api/cron/update-restaurant-status/route.ts
@@ -1,9 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma/client";
 import { getRestaurantBusinessStatus } from "@/lib/places-api";
 import { Restaurant } from "@prisma/client";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
   const restaurants = await prisma.restaurant.findMany();
   const updateRestaurantStatus = async (restaurant: Restaurant) => {
     const { currentOpeningHours } = await getRestaurantBusinessStatus({
