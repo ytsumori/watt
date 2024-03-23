0bd6537516b4ee90ba415e6aeaf29041c61fb62a
miso-devel <kengo279@icloud.com>
2024-03-23 17:50:00 +0900

    add updateManyOrdersIsDownloaded in csv download action

--- app/admin/orders/_components/OrdersCsvDownloadButton/index.tsx
+++ app/admin/orders/_components/OrdersCsvDownloadButton/index.tsx
@@ -4,17 +4,23 @@ import { FC } from "react";
 import { ConvertedOrderInfo } from "../../_util/convertRequiredOrderInfo";
 import { convertToBlob } from "./util";
 import { getCsvBankRecords } from "./action";
+import { updateManyOrdersIsDownloaded } from "@/actions/order";
 
 type OrdersCsvDownloadButtonProps = { orders: ConvertedOrderInfo[] };
 export const OrdersCsvDownloadButton: FC<OrdersCsvDownloadButtonProps> = ({ orders }) => {
   const onClick = async () => {
-    const bankRecords = await getCsvBankRecords(orders);
-    const blob = convertToBlob(bankRecords);
-    const link = document.createElement("a");
-    link.download = "download.csv";
-    link.href = URL.createObjectURL(blob);
-    link.click();
-    URL.revokeObjectURL(link.href);
+    const orderIds = orders.map((order) => order.id);
+    await updateManyOrdersIsDownloaded(orderIds)
+      .then(async () => {
+        const bankRecords = await getCsvBankRecords(orders);
+        const blob = convertToBlob(bankRecords);
+        const link = document.createElement("a");
+        link.download = "download.csv";
+        link.href = URL.createObjectURL(blob);
+        link.click();
+        URL.revokeObjectURL(link.href);
+      })
+      .catch((e) => console.error(e));
   };
 
   return (
