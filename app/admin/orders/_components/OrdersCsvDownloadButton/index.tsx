f238b7d8434122cb9b0b268b09f448e1da072886
miso-devel <kengo279@icloud.com>
2024-03-29 22:03:17 +0900

    add loading and refresh

--- app/admin/orders/_components/OrdersCsvDownloadButton/index.tsx
+++ app/admin/orders/_components/OrdersCsvDownloadButton/index.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { Button } from "@chakra-ui/react";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ConvertedOrderInfo } from "../../_util/convertRequiredOrderInfo";
 import { convertToBlob, isValidOrders } from "./util";
 import { getCsvBankRecords } from "./action";
 import { updateManyOrdersIsDownloaded } from "@/actions/order";
+import { useRouter } from "next/navigation";
 
 type OrdersCsvDownloadButtonProps = { orders: ConvertedOrderInfo[] };
 export const OrdersCsvDownloadButton: FC<OrdersCsvDownloadButtonProps> = ({ orders }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   const filteredOrders = orders.filter(isValidOrders);
   const orderIds = filteredOrders.map((order) => order.id);
   const onClick = async () => {
     if (orderIds.length === 0) return;
+    setIsLoading(true);
     await updateManyOrdersIsDownloaded(orderIds)
       .then(async () => {
         const bankRecords = await getCsvBankRecords(filteredOrders);
@@ -23,11 +27,13 @@ export const OrdersCsvDownloadButton: FC<OrdersCsvDownloadButtonProps> = ({ orde
         URL.revokeObjectURL(link.href);
       })
       .catch((e) => console.error(e));
+    setIsLoading(false);
+    router.refresh();
   };
 
   return (
-    <Button onClick={onClick} marginLeft="auto" minWidth="auto" isDisabled={orderIds.length === 0}>
-      CSVダウンロード
+    <Button onClick={onClick} marginLeft="auto" minWidth="auto" isDisabled={orderIds.length === 0 || isLoading}>
+      {isLoading ? "処理中" : "CSVダウンロード"}
     </Button>
   );
 };
