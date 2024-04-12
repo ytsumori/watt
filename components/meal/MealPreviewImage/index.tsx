d697a7198487ad3c28cec3a57c567a8db6dad68d
miso-devel <kengo279@icloud.com>
2024-04-12 17:36:22 +0900

    change to imagePath

--- components/meal/MealPreviewImage/index.tsx
+++ components/meal/MealPreviewImage/index.tsx
@@ -4,13 +4,11 @@ import { transformSupabaseImage } from "@/utils/image/transformSupabaseImage";
 import { Image, ImageProps } from "@chakra-ui/react";
 
 type Props = {
-  src: ImageProps["src"];
+  src: ImageProps["src"] | null;
   alt: ImageProps["alt"];
 };
 
 export function MealPreviewImage({ src, alt }: Props) {
-  if (src === undefined) return <Image src={src} alt={alt} objectFit="cover" aspectRatio={1 / 1} borderRadius={8} />;
-
   const publicUrl = transformSupabaseImage("meals", src);
 
   return <Image src={publicUrl} alt={alt} objectFit="cover" aspectRatio={1 / 1} borderRadius={8} />;
