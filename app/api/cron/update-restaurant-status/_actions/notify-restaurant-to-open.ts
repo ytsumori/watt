67a2d5b8d10684726e3546959d80128862b6abe7
Yuta Tsumori <yutatsumori@gmail.com>
2024-04-23 16:00:52 +0900

    Update reminder message

--- app/api/cron/update-restaurant-status/_actions/notify-restaurant-to-open.ts
+++ app/api/cron/update-restaurant-status/_actions/notify-restaurant-to-open.ts
@@ -11,7 +11,15 @@ export async function notifyRestaurantToOpen({ restaurantId }: { restaurantId: s
   });
 
   const notify = async (staffLineId: string) => {
-    pushMessage({ to: staffLineId, messages: [{ type: "text", text: "Please open the restaurant" }] });
+    pushMessage({
+      to: staffLineId,
+      messages: [
+        {
+          type: "text",
+          text: `来店不可の状態が続いております。\n\n来店が可能な場合は設定画面よりステータスの更新をお願いいたします。\n${process.env.NEXT_PUBLIC_LIFF_URL}`
+        }
+      ]
+    });
   };
   await Promise.all(staffs.map((staff) => notify(staff.lineId)));
 }
