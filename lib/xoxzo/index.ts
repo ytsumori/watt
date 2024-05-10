e83b09a5cc675d82b8af2f2718a02caab33d89c3
Yuta Tsumori <yutatsumori@gmail.com>
2024-05-10 09:48:49 +0900

    Add voice call function

--- lib/xoxzo/index.ts
+++ lib/xoxzo/index.ts
@@ -44,3 +44,27 @@ export async function sendMessage(to: string, message: string) {
     return 500;
   }
 }
+
+export async function sendVoiceCall(to: string, audioUrl: string) {
+  const data = {
+    caller: "+81665332030",
+    recipient: `+81${to}`,
+    recording_url: audioUrl
+  };
+  try {
+    const response = await fetch("https://api.xoxzo.com/voice/simple/playbacks/", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${btoa(`${process.env.XOXZO_API_SID}:${process.env.XOXZO_API_TOKEN}`)}`
+      },
+      method: "POST",
+      body: JSON.stringify(data)
+    });
+    console.log(await response.json());
+    return response.status;
+  } catch (e) {
+    console.log("call missed");
+    console.error(e);
+    return 500;
+  }
+}
