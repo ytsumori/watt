1a663db67b0b04c81ec2da85f37458849d4ee87b
Yuta Tsumori <yutatsumori@gmail.com>
2024-05-10 16:25:39 +0900

    bugfix: wrong redirect path

--- app/admin/payments/_components/DateRangeEditor/action.ts
+++ app/admin/payments/_components/DateRangeEditor/action.ts
@@ -4,5 +4,5 @@ import { redirect } from "next/navigation";
 export const onSubmit = (formData: FormData) => {
   const start = formData.get("start");
   const end = formData.get("end");
-  redirect(`/admin/orders?start=${start}&end=${end}`);
+  redirect(`/admin/payments?start=${start}&end=${end}`);
 };
