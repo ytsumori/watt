707152c6f12d742056903cc19f1405ae6fbe1c2c
Yuta Tsumori <yutatsumori@gmail.com>
2024-05-17 10:48:27 +0900

    reinstall packages

--- app/(user-app)/_components/HomePage/index.tsx
+++ app/(user-app)/_components/HomePage/index.tsx
@@ -106,7 +106,7 @@ export default function HomePage({
                           )}
                         </>
                       ) : (
-                        <Skeleton height="12px" w="full" />
+                        <Skeleton height="12px" w="50%" />
                       )}
                     </>
                   )}
