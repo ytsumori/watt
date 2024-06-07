76e7ad73100374318da2df40034fdfdc6f6118db
miso-devel <kengo279@icloud.com>
2024-06-07 18:48:23 +0900

    add type to supabase client

--- lib/supabase/createServiceRoleClient.ts
+++ lib/supabase/createServiceRoleClient.ts
@@ -1,5 +1,6 @@
+import { Database } from "@/types/supabase";
 import { createClient } from "@supabase/supabase-js";
 
 export const createServiceRoleClient = () => {
-  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!);
+  return createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!);
 };
