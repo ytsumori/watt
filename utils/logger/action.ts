aef45844e2bceee1de130f2cc8980567f3699463
Yuta Tsumori <yutatsumori@gmail.com>
2024-06-10 16:19:39 +0900

    fix logging action

--- utils/logger/action.ts
+++ utils/logger/action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-export const logAction = ({ severity, isClient, message, payload }: LoggerArgs) => {
+export const logAction = async ({ severity, isClient, message, payload }: LoggerArgs) => {
   const info = { ...payload, severity, message, isClient };
-  console.log(JSON.stringify(info));
+  console.error(JSON.stringify(info));
 };
