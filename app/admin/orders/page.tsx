fe20d3bd17f4b04bef7e6f27009bffe04d54dccd
Yuta Tsumori <yutatsumori@gmail.com>
2024-06-14 17:49:51 +0900

    add orders page

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,45 @@
+import prisma from "@/lib/prisma/client";
+import { OrdersPage } from "./_components/OrdersPage";
+
+type Props = {
+  searchParams: {
+    page: string;
+  };
+};
+
+const PAGE_SIZE = 20;
+export default async function Orders({ searchParams }: Props) {
+  const page = parseInt(searchParams.page) || 1;
+  const skip = (page - 1) * PAGE_SIZE;
+  const orders = await prisma.order.findMany({
+    select: {
+      id: true,
+      orderNumber: true,
+      peopleCount: true,
+      completedAt: true,
+      meals: {
+        select: {
+          id: true,
+          meal: { select: { title: true, price: true } },
+          options: { select: { mealItemOption: { select: { title: true, extraPrice: true } } } },
+          quantity: true
+        }
+      },
+      payment: { select: { totalAmount: true } },
+      restaurant: { select: { id: true, name: true } }
+    },
+    where: {
+      completedAt: {
+        not: null
+      }
+    },
+    orderBy: {
+      completedAt: "desc"
+    },
+    take: PAGE_SIZE,
+    skip
+  });
+  const count = await prisma.order.count();
+  const maxPage = Math.ceil(count / PAGE_SIZE);
+  return <OrdersPage orders={orders} page={page} maxPage={maxPage} />;
+}
