472879393a4ba4af893ffe3026d4ba6c0a7066b6
Yuta Tsumori <yutatsumori@gmail.com>
2024-06-24 15:37:36 +0900

    remove payment step from onboarding modal

--- app/(user-app)/_components/OnboardingModal/index.tsx
+++ app/(user-app)/_components/OnboardingModal/index.tsx
@@ -3,7 +3,6 @@
 import {
   Box,
   Button,
-  Center,
   HStack,
   Heading,
   Modal,
@@ -25,24 +24,19 @@ import { useEffect, useState } from "react";
 import onboarding1Image from "@/public/onboarding/1.png";
 import onboarding2Image from "@/public/onboarding/2.png";
 import onboarding3Image from "@/public/onboarding/3.png";
-import onboarding4Image from "@/public/onboarding/4.png";
 
 const ONBOARDING_STEPS = [
   {
-    text: "今すぐ入れるお店から\n行きたいお店を探す",
+    text: "今すぐ入れるお店から行きたいお店を探す",
     imageSrc: onboarding1Image
   },
   {
-    text: "「お店に向かうボタン」を\n押してお店に来店",
+    text: "「お店に向かうボタン」を押してお店に来店",
     imageSrc: onboarding2Image
   },
   {
-    text: "お得なセットで\n食事をする",
+    text: "お得なセットで食事をする",
     imageSrc: onboarding3Image
-  },
-  {
-    text: "Wattでカード決済\n現金支払いも可能",
-    imageSrc: onboarding4Image
   }
 ];
 
@@ -63,7 +57,7 @@ export function OnboardingModal() {
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={() => undefined} isCentered>
+    <Modal isOpen={isOpen} onClose={() => undefined} isCentered size="full">
       <ModalOverlay />
       <ModalContent>
         <ModalHeader alignSelf="center">
@@ -74,61 +68,64 @@ export function OnboardingModal() {
             </Heading>
           </HStack>
         </ModalHeader>
-        <ModalBody>
-          <Center>
-            <VStack>
-              <Heading whiteSpace="pre-line" textAlign="center">
-                {ONBOARDING_STEPS[currentStep].text}
-              </Heading>
+        <ModalBody alignContent="center">
+          <VStack m="auto">
+            <Heading whiteSpace="pre-line" textAlign="center">
+              {ONBOARDING_STEPS[currentStep].text}
+            </Heading>
+            {ONBOARDING_STEPS.map((step, index) => (
               <Image
-                src={ONBOARDING_STEPS[currentStep].imageSrc}
-                alt={`ステップ${currentStep + 1}`}
+                key={index}
+                src={step.imageSrc}
+                alt={`ステップ${index + 1}`}
                 width="400"
                 height="400"
+                style={{ display: index === currentStep ? "block" : "none" }}
               />
-            </VStack>
-          </Center>
+            ))}
+          </VStack>
         </ModalBody>
         <ModalFooter as={VStack} spacing={10}>
-          <Box w="full" position="relative" display="flex" h="32px">
-            <Button
-              onClick={() => setCurrentStep(currentStep - 1)}
-              position="absolute"
-              left={0}
-              variant="outline"
-              isDisabled={currentStep === 0}
-            >
-              前へ
-            </Button>
-            <Stepper index={currentStep + 1} size="sm" gap="0" mx="auto">
-              {ONBOARDING_STEPS.map((_, index) => (
-                <Step key={index}>
-                  <StepIndicator
-                    sx={{
-                      "[data-status=active] &": {
-                        borderColor: "gray.200"
-                      }
-                    }}
-                  >
-                    <StepStatus active={<></>} complete={<StepIcon />} />
-                  </StepIndicator>
-                  <StepSeparator />
-                </Step>
-              ))}
-            </Stepper>
-            <Button
-              onClick={() => setCurrentStep(currentStep + 1)}
-              position="absolute"
-              right={0}
-              isDisabled={currentStep === ONBOARDING_STEPS.length - 1}
-            >
-              次へ
-            </Button>
-          </Box>
-          {currentStep === ONBOARDING_STEPS.length - 1 && (
+          {currentStep === ONBOARDING_STEPS.length - 1 ? (
             <Button onClick={handleOnSubmit} size="lg" mx="auto">
               使い始める
             </Button>
+          ) : (
+            <Box w="full" position="relative" display="flex" h="32px">
+              <Button
+                onClick={() => setCurrentStep(currentStep - 1)}
+                position="absolute"
+                left={0}
+                variant="outline"
+                isDisabled={currentStep === 0}
+              >
+                前へ
+              </Button>
+              <Stepper index={currentStep + 1} size="sm" gap="0" mx="auto">
+                {ONBOARDING_STEPS.map((_, index) => (
+                  <Step key={index}>
+                    <StepIndicator
+                      sx={{
+                        "[data-status=active] &": {
+                          borderColor: "gray.200"
+                        }
+                      }}
+                    >
+                      <StepStatus active={<></>} complete={<StepIcon />} />
+                    </StepIndicator>
+                    <StepSeparator />
+                  </Step>
+                ))}
+              </Stepper>
+              <Button
+                onClick={() => setCurrentStep(currentStep + 1)}
+                position="absolute"
+                right={0}
+                isDisabled={currentStep === ONBOARDING_STEPS.length - 1}
+              >
+                次へ
+              </Button>
+            </Box>
           )}
         </ModalFooter>
       </ModalContent>
