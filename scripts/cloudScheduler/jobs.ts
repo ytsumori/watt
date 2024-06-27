66ab9eab96b5a96679c4bb273ec48939446fed6e
miso-devel <kengo279@icloud.com>
2024-06-27 17:29:01 +0900

    fix jobs

--- scripts/cloudScheduler/jobs.ts
+++ scripts/cloudScheduler/jobs.ts
@@ -3,21 +3,23 @@ import { protos } from "@google-cloud/scheduler";
 const base = {
   name: "projects/" + process.env.GCP_PROJECT_ID + "/locations/" + process.env.CLOUD_SCHEDULER_LOCATION + "/jobs/",
   timeZone: "Asia/Tokyo",
-  url: process.env.NEXT_PUBLIC_HOST_URL + "/api/cron"
+  url: process.env.NEXT_PUBLIC_HOST_URL + "/api/cron",
+  "Content-Type": "application/json",
+  authorization: `Bearer ${process.env.CRON_SECRET}`
 };
 
 export const jobs: protos.google.cloud.scheduler.v1.IJob[] = [
   {
     name: base.name + "sample-job-1",
     schedule: "*/5 * * * *",
-    description: "サンプルスケジューラー",
-    timeZone: "Asia/Tokyo",
+    description: "サンプルジョブ",
+    timeZone: base.timeZone,
     httpTarget: {
       uri: base.url + "/sample-job-1",
       httpMethod: "GET",
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.CRON_SECRET}`
+        "Content-Type": base["Content-Type"],
+        Authorization: base.authorization
       }
     }
   }
