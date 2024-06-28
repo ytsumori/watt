ec714a1490dd2d839311a2a856ab197882d06542
miso-devel <kengo279@icloud.com>
2024-06-28 16:13:32 +0900

    fix sample job

--- scripts/cloudScheduler/jobs.ts
+++ scripts/cloudScheduler/jobs.ts
@@ -2,6 +2,5 @@ import { protos } from "@google-cloud/scheduler";
 import { createJob } from "./utils";
 
 export const jobs: protos.google.cloud.scheduler.v1.IJob[] = [
-  createJob({ name: "sample-job-1", description: "サンプルジョブ", schedule: "*/45 * * * *", path: "/sample-job-1" }),
-  createJob({ name: "sample-job-2", description: "サンプルジョブ2", schedule: "*/1 * * * *", path: "/sample-job-2" })
+  createJob({ name: "sample-job-1", description: "サンプルジョブ", schedule: "*/45 * * * *", path: "/sample-job-1" })
 ];
