34e14798b321682876d9213b53ad11806958a26f
miso-devel <kengo279@icloud.com>
2024-06-28 16:09:33 +0900

    fix deploy process

--- scripts/cloudScheduler/deploy.ts
+++ scripts/cloudScheduler/deploy.ts
@@ -4,7 +4,7 @@ import { protos } from "@google-cloud/scheduler";
 import { jobs } from "./jobs";
 import { logger } from "@/utils/logger";
 
-const createCloudSchedulerJob = async () => {
+(async () => {
   const client = new scheduler.CloudSchedulerClient({
     credentials: {
       client_email: process.env.CLOUD_SCHEDULER_AUTH_EMAIL,
@@ -27,18 +27,15 @@ const createCloudSchedulerJob = async () => {
     jobs.filter((job) => existJobNames.includes(job.name)).map((job) => client.updateJob({ job }));
 
   const createJob = () =>
-    jobs.filter((job) => !existJobNames.includes(job.name)).map(async (job) => client.createJob({ parent, job }));
+    jobs.filter((job) => !existJobNames.includes(job.name)).map((job) => client.createJob({ parent, job }));
 
   const deleteJob = () =>
     existJobs[0]
       .filter((existJob) => !jobNames.includes(existJob.name))
       .map((job) => client.deleteJob({ name: job.name }));
 
-  return await Promise.allSettled([...updateJob(), ...createJob(), ...deleteJob()]);
-};
+  const results = await Promise.allSettled([...updateJob(), ...createJob(), ...deleteJob()]);
 
-(async () => {
-  const results = await createCloudSchedulerJob();
   results?.forEach((result) => {
     if (result.status === "fulfilled") {
       const value = result.value[0] as protos.google.cloud.scheduler.v1.IJob;
