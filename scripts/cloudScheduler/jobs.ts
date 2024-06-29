00a8f519b6cac92cbc812f7f66f0cbc58136baa8
miso-devel <kengo279@icloud.com>
2024-06-29 14:57:10 +0900

    vercer cron to cloud scheduler

--- scripts/cloudScheduler/jobs.ts
+++ scripts/cloudScheduler/jobs.ts
@@ -2,5 +2,41 @@ import { protos } from "@google-cloud/scheduler";
 import { createJob } from "./utils";
 
 export const jobs: protos.google.cloud.scheduler.v1.IJob[] = [
-  createJob({ name: "sample-job-1", description: "サンプルジョブ", schedule: "*/45 * * * *", path: "/sample-job-1" })
+  createJob({ name: "sample-job-1", description: "サンプルジョブ", schedule: "*/45 * * * *", path: "/sample-job-1" }),
+  createJob({
+    name: "update-restaurant-status",
+    description: "レストランのステータスを更新するジョブ",
+    schedule: "*/30 * * * *",
+    path: "/update-restaurant-status"
+  }),
+  createJob({
+    name: "update-opening-hours",
+    description: "営業時間を更新するジョブ",
+    schedule: "15 21 * * *",
+    path: "/update-opening-hours"
+  }),
+  createJob({
+    name: "update-map-place-detail",
+    description: "'レストランの場所を更新するジョブ",
+    schedule: "15 21 1 * *",
+    path: "/update-map-place-detail"
+  }),
+  createJob({
+    name: "notify-closing-restaurants",
+    description: "閉店するレストランを通知するジョブ",
+    schedule: "0 3 * * *",
+    path: "/notify-closing-restaurants"
+  }),
+  createJob({
+    name: "update-restaurant-coordinates",
+    description: "レストランの座標を更新するジョブ",
+    schedule: "15 21 1 * *",
+    path: "/update-restaurant-coordinates"
+  }),
+  createJob({
+    name: "verify-order-price",
+    description: "注文金額を検証するジョブ",
+    schedule: "0 15 * * *",
+    path: "/verify-order-price"
+  })
 ];
