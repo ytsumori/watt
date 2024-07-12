5f4e004e10de393a8f4cd0d76291c3fdc7cba67e
Yuta Tsumori <yutatsumori@gmail.com>
2024-07-12 15:29:09 +0900

    remove unnecessary tests

--- test/util/createRandomId.ts
+++
@@ -1,6 +0,0 @@
-export const createRandomStr = (length?: number) => {
-  const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  return Array.from(Array(length ?? 20))
-    .map(() => S[Math.floor(Math.random() * S.length)])
-    .join("");
-};
