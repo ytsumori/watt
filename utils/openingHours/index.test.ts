b6eea317fe97612c9e1c8f3b0b639a7271db1197
miso-devel <kengo279@icloud.com>
2024-07-20 13:36:20 +0900

    fix day change

--- utils/openingHours/index.test.ts
+++ utils/openingHours/index.test.ts
@@ -6,8 +6,10 @@ describe("[utils/openingHours]", () => {
   const mock: OpeningHoursResult["currentOpeningHours"] = {
     periods: [
       { open: { day: 0, hour: 0, minute: 0 }, close: { day: 0, hour: 23, minute: 59 } },
-      { open: { day: 0, hour: 0, minute: 0 }, close: { day: 0, hour: 0, minute: 0 } },
-      { open: { day: 0, hour: 0, minute: 0 }, close: { day: 0, hour: 19, minute: 10 } }
+      { open: { day: 1, hour: 17, minute: 0 }, close: { day: 2, hour: 0, minute: 0 } },
+      { open: { day: 2, hour: 17, minute: 0 }, close: { day: 2, hour: 19, minute: 10 } },
+      { open: { day: 6, hour: 17, minute: 0 }, close: { day: 6, hour: 22, minute: 59 } },
+      { open: { day: 6, hour: 17, minute: 0 }, close: { day: 6, hour: 23, minute: 59 } }
     ]
   };
 
@@ -15,9 +17,11 @@ describe("[utils/openingHours]", () => {
     const converted = convertOpeningHours(mock);
     expect(converted).toEqual({
       periods: [
-        { open: { day: 0, hour: 0, minute: 0 }, close: { day: 0, hour: 0, minute: 0 } },
-        { open: { day: 0, hour: 0, minute: 0 }, close: { day: 0, hour: 0, minute: 0 } },
-        { open: { day: 0, hour: 0, minute: 0 }, close: { day: 0, hour: 19, minute: 10 } }
+        { open: { day: 0, hour: 0, minute: 0 }, close: { day: 1, hour: 0, minute: 0 } },
+        { open: { day: 1, hour: 17, minute: 0 }, close: { day: 2, hour: 0, minute: 0 } },
+        { open: { day: 2, hour: 17, minute: 0 }, close: { day: 2, hour: 19, minute: 10 } },
+        { open: { day: 6, hour: 17, minute: 0 }, close: { day: 6, hour: 23, minute: 0 } },
+        { open: { day: 6, hour: 17, minute: 0 }, close: { day: 0, hour: 0, minute: 0 } }
       ]
     });
   });
--- utils/openingHours/index.ts
+++ utils/openingHours/index.ts
@@ -5,15 +5,24 @@ export const convertOpeningHours = (
 ): OpeningHoursResult["currentOpeningHours"] => {
   return currentOpeningHours
     ? {
-        periods: currentOpeningHours.periods.map((period) => ({
-          ...period,
-          close: {
-            ...period.close,
-            hour:
-              period.close.minute === 59 ? (period.close.hour === 23 ? 0 : period.close.hour + 1) : period.close.hour,
-            minute: period.close.minute === 59 ? 0 : period.close.minute
-          }
-        }))
+        periods: currentOpeningHours.periods.map((period) => {
+          const shouldChangeMinute = period.close.minute === 59;
+          const shouldChangeHour = period.close.hour === 23;
+          return {
+            ...period,
+            close: {
+              ...period.close,
+              day:
+                shouldChangeMinute && shouldChangeHour
+                  ? period.close.day === 6
+                    ? 0
+                    : period.close.day + 1
+                  : period.close.day,
+              hour: shouldChangeMinute ? (shouldChangeHour ? 0 : period.close.hour + 1) : period.close.hour,
+              minute: shouldChangeMinute ? 0 : period.close.minute
+            }
+          };
+        })
       }
     : currentOpeningHours;
 };
