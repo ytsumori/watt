0d85ef887694fb01a912aa1746ab6dd8536ea60f
miso-devel <kengo279@icloud.com>
2024-09-20 17:13:23 +0900

    fix update cron condition

--- app/api/cron/update-restaurant-status/route.ts
+++ app/api/cron/update-restaurant-status/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma/client";
 import { Prisma } from "@prisma/client";
-import { isCurrentlyWorkingHour } from "@/utils/opening-hours";
+import { isCurrentlyWorkingHour, mergeOpeningHours } from "@/utils/opening-hours";
 
 export async function GET(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
@@ -12,34 +12,64 @@ export async function GET(request: NextRequest) {
   }
 
   const restaurants = await prisma.restaurant.findMany({
-    include: { meals: true, manualCloses: { take: 1, orderBy: { createdAt: "desc" } } },
+    include: {
+      meals: true,
+      manualCloses: { take: 1, orderBy: { createdAt: "desc" } },
+      openingHours: true,
+      holidays: { include: { openingHours: true } }
+    },
     where: { isPublished: true, meals: { some: { isInactive: false } } }
   });
 
   const update = async (
-    restaurant: Prisma.RestaurantGetPayload<{ include: { manualCloses: { take: 1; orderBy: { createdAt: "desc" } } } }>
+    restaurant: Prisma.RestaurantGetPayload<{
+      include: {
+        manualCloses: { take: 1; orderBy: { createdAt: "desc" } };
+        openingHours: true;
+        holidays: { include: { openingHours: true } };
+      };
+    }>
   ) => {
+    const openingHours = mergeOpeningHours({
+      regularOpeningHours: restaurant.openingHours,
+      holidays: restaurant.holidays
+    });
+
+    if (openingHours.length === 0) return;
+
     const manualCloses = restaurant.manualCloses;
-    if (manualCloses.length === 0) {
-      await prisma.restaurant.update({ where: { id: restaurant.id }, data: { isAvailable: true } });
+
+    if (isCurrentlyWorkingHour(openingHours)) {
+      if (manualCloses.length === 0) {
+        await prisma.restaurant.update({ where: { id: restaurant.id }, data: { isAvailable: true } });
+        return NextResponse.json({ success: true });
+      } else {
+        const closedHour = manualCloses[0].googleMapOpeningHourId
+          ? await prisma.restaurantGoogleMapOpeningHour.findUnique({
+              where: { id: manualCloses[0].googleMapOpeningHourId }
+            })
+          : manualCloses[0].holidayOpeningHourId &&
+            (await prisma.restaurantHolidayOpeningHour.findUnique({
+              where: { id: manualCloses[0].holidayOpeningHourId }
+            }));
+
+        // 現在が前回手動で閉めた時間帯でない場合は営業中に変更
+        if (closedHour && !isCurrentlyWorkingHour([closedHour])) {
+          prisma.$transaction(async (tx) => {
+            await tx.restaurantManualClose.delete({ where: { id: manualCloses[0].id } });
+            await tx.restaurant.update({ where: { id: restaurant.id }, data: { isAvailable: true } });
+          });
+        }
+      }
+    } else {
+      prisma.$transaction(async (tx) => {
+        manualCloses.length !== 0 && (await tx.restaurantManualClose.delete({ where: { id: manualCloses[0].id } }));
+        await tx.restaurant.update({ where: { id: restaurant.id }, data: { isAvailable: false } });
+      });
       return NextResponse.json({ success: true });
     }
-
-    const closedHour = manualCloses[0].googleMapOpeningHourId
-      ? await prisma.restaurantGoogleMapOpeningHour.findUnique({
-          where: { id: manualCloses[0].googleMapOpeningHourId }
-        })
-      : manualCloses[0].holidayOpeningHourId &&
-        (await prisma.restaurantHolidayOpeningHour.findUnique({ where: { id: manualCloses[0].holidayOpeningHourId } }));
-
-    // 現在が前回手動で閉めた時間帯でない場合は営業中に変更
-    if (closedHour && !isCurrentlyWorkingHour([closedHour])) {
-      prisma.$transaction(async (tx) => {
-        await tx.restaurantManualClose.delete({ where: { id: manualCloses[0].id } });
-        await tx.restaurant.update({ where: { id: restaurant.id }, data: { isAvailable: true } });
-      });
-    }
   };
+
   await Promise.all(restaurants.map(update));
 
   return NextResponse.json({ success: true });
