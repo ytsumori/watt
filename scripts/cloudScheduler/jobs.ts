1670a0495c62fb2769cd37b083b852ae87235828
Yuta Tsumori <yutatsumori@gmail.com>
2024-09-21 11:12:08 +0900

    remove unused job from scheduler

--- scripts/cloudScheduler/jobs.ts
+++ scripts/cloudScheduler/jobs.ts
@@ -21,12 +21,6 @@ export const jobs: protos.google.cloud.scheduler.v1.IJob[] = [
     path: "/update-map-place-detail"
   }),
   createJob({
-    name: "notify-closing-restaurants",
-    description: "閉店するレストランを通知するジョブ",
-    schedule: "0 3 * * *",
-    path: "/notify-closing-restaurants"
-  }),
-  createJob({
     name: "update-restaurant-coordinates",
     description: "レストランの座標を更新するジョブ",
     schedule: "15 21 1 * *",
