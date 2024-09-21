a7e56e5a427be3e27240bf6c6b9b04951d1061c9
miso-devel <kengo279@icloud.com>
2024-09-21 15:09:01 +0900

    fix sort

--- app/(user-app)/_components/RestaurantHalfModalBody/util.ts
+++ app/(user-app)/_components/RestaurantHalfModalBody/util.ts
@@ -10,7 +10,18 @@ export const groupedByDayOfWeeks = (
   const groupedByDayOfWeeks = Object.groupBy(openingHours, (openingHour) => openingHour.openDayOfWeek);
   return Object.keys(DayOfWeek).map((dayOfWeek) => {
     return groupedByDayOfWeeks[dayOfWeek as keyof typeof DayOfWeek]
-      ? `${translateDayOfWeek(dayOfWeek as keyof typeof DayOfWeek)} ${groupedByDayOfWeeks[dayOfWeek as keyof typeof DayOfWeek]?.map((openingHour) => `${String(openingHour.openHour).padStart(2, "0")}:${String(openingHour.openMinute).padStart(2, "0")} ~ ${String(openingHour.closeHour).padStart(2, "0")}:${String(openingHour.closeMinute).padStart(2, "0")}`).join(" / ")}`
+      ? `${translateDayOfWeek(dayOfWeek as keyof typeof DayOfWeek)} ${groupedByDayOfWeeks[
+          dayOfWeek as keyof typeof DayOfWeek
+        ]
+          ?.sort((a, b) => {
+            if (a.openHour === b.openHour) return a.openMinute > b.openMinute ? 1 : -1;
+            return a.openHour > b.openHour ? 1 : -1;
+          })
+          .map(
+            (openingHour) =>
+              `${String(openingHour.openHour).padStart(2, "0")}:${String(openingHour.openMinute).padStart(2, "0")} ~ ${String(openingHour.closeHour).padStart(2, "0")}:${String(openingHour.closeMinute).padStart(2, "0")}`
+          )
+          .join(" / ")}`
       : `${translateDayOfWeek(dayOfWeek as keyof typeof DayOfWeek)} 営業時間外`;
   });
 };
